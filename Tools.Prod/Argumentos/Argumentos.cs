45130a8fed0b5293a43e5a13aaabbc55c56ef1c2
joaqfer <joaquin.fdez.ruiz@gmail.com>
2014-12-12 00:08:31 +0100

    Test Verde DevolverVerdaderoSiNumeroArgumentosIgualAlEsperado

---
+++ Tools.Prod/Argumentos/Argumentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tools.Prod.Argumentos
+{
+  public class Argumentos
+  {
+    private string[] args;
+
+    public Argumentos(string[] args)
+    {
+      this.args = args;
+    }
+
+
+
+    public bool ValidarNumArgumentos(int numArgsEsperado)
+    {
+      return true;
+    }
+  }
+}
---
+++ Tools.Test/ArgumentosTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Tools.Prod.Argumentos;
+
+namespace Tools.Test
+
+{
+  [TestFixture]
+  public class ArgumentosTests
+  {
+    [Test]
+    public void DevolverVerdaderoSiNumeroArgumentosIgualAlEsperado()
+    {
+      var argumentos = new Argumentos(new string[] { "un argumento" });
+      Assert.IsTrue(argumentos.ValidarNumArgumentos(1));
+    }
+  }
+}

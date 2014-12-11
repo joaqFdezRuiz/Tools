5ff77d40bac446460f709808f030c30cb6d1226f
joaqfer <joaquin.fdez.ruiz@gmail.com>
2014-12-12 00:42:27 +0100

    Test Verde DevolverCeroSiFinalizacionCorrecta

---
+++ Tools.Test/Finalizacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tools.Prod
+{
+  public class Finalizacion
+  {
+    public int Correcto
+    {
+      get { return 0; }
+    }
+  }
+}
---
+++ Tools.Test/FinalizacionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tools.Test
+{
+  [TestFixture]
+  class FinalizacionTests
+  {
+    [Test]
+    public void DevolverCeroSiFinalizacionCorrecta() {
+      Tools.Prod.Finalizacion finalizacion = new Tools.Prod.Finalizacion();
+      Assert.AreEqual(0, finalizacion.Correcto);
+    }
+  }
+}
